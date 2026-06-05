0046694225f952c181c8a363f5a92c9d832e19d8
o51r15 <o51r15@users.noreply.github.com>
2026-06-05 19:13:37 -0400

    Migrate uploaded_at from date to timestamptz

---
+++ priv/repo/migrations/20260605120000_change_uploaded_at_to_utc_datetime.exs
@@ -0,0 +1,11 @@
+defmodule Pinchflat.Repo.Migrations.ChangeUploadedAtToUtcDatetime do
+  use Ecto.Migration
+
+  def up do
+    execute("ALTER TABLE media_items ALTER COLUMN uploaded_at TYPE timestamptz USING uploaded_at::timestamptz")
+  end
+
+  def down do
+    execute("ALTER TABLE media_items ALTER COLUMN uploaded_at TYPE date USING uploaded_at::date")
+  end
+end
