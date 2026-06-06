20ebadfaf944e76fc1e534405554fd9bca1881aa
o51r15 <o51r15@users.noreply.github.com>
2026-06-06 14:13:32 -0400

    Change last_error to text type to handle long error messages

---
+++ priv/repo/migrations/20260606000004_change_last_error_to_text.exs
@@ -0,0 +1,11 @@
+defmodule Pinchflat.Repo.Migrations.ChangeLastErrorToText do
+  use Ecto.Migration
+
+  def up do
+    execute("ALTER TABLE media_items ALTER COLUMN last_error TYPE text")
+  end
+
+  def down do
+    execute("ALTER TABLE media_items ALTER COLUMN last_error TYPE varchar(255) USING left(last_error, 255)")
+  end
+end
